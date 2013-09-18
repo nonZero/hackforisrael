0e8cc73139faa45991e7d00fb9a3749470a871b2
Udi Oron <udioron@gmail.com>
2013-09-18 08:59:40 +0300

    cleanup / PEP8

--- src/hashmabir/views.py
+++ src/hashmabir/views.py
@@ -1,18 +1,16 @@
-from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView
 from django.views.generic import ListView, DetailView
 
-import os.path
-import re
-
-from models import Hashmabir
+from hashmabir.models import Hashmabir
 
 
 class HashmabirListView(ListView):
     model = Hashmabir
 
+
 class HashmabirCreateView(CreateView):
     model = Hashmabir
+
 
 class HashmabirDetailView(DetailView):
     model = Hashmabir
