1220f1d4f404426d745dd82311eb5982d4e03abf
Yoni Yalovitsky <yoni@fewbytes.com>
2013-10-08 00:21:19 +0300

    student_application progression

--- src/student_applications/views.py
+++ src/student_applications/views.py
@@ -1,7 +1,9 @@
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.shortcuts import redirect
 from django.utils.translation import gettext_lazy as _
-from django.views.generic.base import TemplateView
+from django.views.generic.base import TemplateView, View
 from django.views.generic.edit import FormView
 from q13es.forms import parse_form
 from q13es.models import Answer
@@ -37,7 +39,6 @@ def get_user_forms(user):
 
 
 def get_user_next_form(user):
-
     filled = get_user_forms(user)
 
     for f in FORM_NAMES:
@@ -47,16 +48,46 @@ def get_user_next_form(user):
     return None
 
 
-class Dashboard(TemplateView):
+def get_user_progress(user):
+    return len(get_user_forms(user)), len(FORMS)
+
+
+class ProtectedMixin(View):
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(ProtectedMixin, self).dispatch(request, *args, **kwargs)
+
+
+class Dashboard(TemplateView, ProtectedMixin):
     template_name = 'dashboard.html'
 
+    def get(self, request, *args, **kwargs):
+        if get_user_next_form(request.user):
+            return redirect('fill_form')
 
-class FillFormView(FormView):
+        return super(Dashboard, self).get(self, request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        d = super(Dashboard, self).get_context_data(**kwargs)
+        d['forms'] = [(k, FORMS[k]) for k in FORM_NAMES]
+        return d
+
+
+class FillFormView(FormView, ProtectedMixin):
     template_name = 'fill-form.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        form = self.get_form_class()
+        if form is None:
+            return redirect('dashboard')
+
+        return form
 
     def get_form_class(self):
         form_name = get_user_next_form(self.request.user)
-        assert form_name
+        if not form_name:
+            return None
+
         return FORMS[form_name]
 
     def form_valid(self, form):
@@ -67,8 +98,13 @@ class FillFormView(FormView):
         messages.info(self.request, _("'%s' was saved.") % form.form_title)
         return redirect('fill_form')
 
+    def get_context_data(self, **kwargs):
+        d = super(FillFormView, self).get_context_data(**kwargs)
+        d['progress'] = _("%d form(s) filled out of %d") % get_user_progress(self.request.user)
+        return d
 
-class AllFormsView(TemplateView):
+
+class AllFormsView(TemplateView, ProtectedMixin):
     template_name = 'all-forms.html'
 
     def get_context_data(self, **kwargs):
