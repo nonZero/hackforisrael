e4ccd3e4c7256617945da741acb66730da23a7c3
Udi Oron <udioron@gmail.com>
2013-10-14 11:03:25 +0300

    oops, bad logging settings

--- src/h4il/settings.py
+++ src/h4il/settings.py
@@ -177,7 +177,7 @@ ACCOUNT_EMAIL_VERIFICATION = "mandatory"
 ACCOUNT_USER_DISPLAY = lambda u: u.email
 ACCOUNT_USERNAME_REQUIRED = False
 
-#SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
+# SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
@@ -202,8 +202,8 @@ LOGGING = {
             'class': 'logging.StreamHandler',
         },
     },
-        'django.request': {
     'loggers': {
+        'django.request': {
             'handlers': ['mail_admins', 'console'],
             'level': 'ERROR',
             'propagate': True,
