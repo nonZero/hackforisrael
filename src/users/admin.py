3562b13b36c81feadb50dafe9cd0019aadfd2a9b
Udi Oron <udioron@gmail.com>
2013-10-15 23:33:51 +0300

    Enable basic user editing via admin, now working

--- src/users/admin.py
+++ src/users/admin.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from users.models import HackitaUser
 from django.utils.translation import ugettext_lazy as _
 
 
-class HackitaUserAdmin(UserAdmin):
+class HackitaUserAdmin(admin.ModelAdmin):
 
     fieldsets = (
-        (None, {'fields': ('username', 'password')}),
         (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
         (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
