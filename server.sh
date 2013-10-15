06f3dff99e6e5c1067984ac52631e903a2a32f36
Udi Oron <udioron@gmail.com>
2013-10-15 18:16:09 +0300

    better unicorn start script

--- server.sh
+++ server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source /home/udi/.virtualenvs/h4il/bin/activate
+source $DIR/activate
 cd $DIR/src
-exec gunicorn -p ~/h4il.pid -b 127.0.0.1:8098 -w 2 h4il.wsgi:application
+exec gunicorn -n hackita -p ~/h4il.pid -b 127.0.0.1:8098 -w 2 h4il.wsgi:application
