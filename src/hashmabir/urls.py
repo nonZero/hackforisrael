63f7a2613f613630c57bfe3ae60a583a7c46b327
Udi Oron <udioron@gmail.com>
2013-10-21 19:07:02 +0300

    cleanup

--- src/hashmabir/urls.py
+++ src/hashmabir/urls.py
@@ -1,8 +1,7 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns,  url
 import views
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', views.HashmabirListView.as_view(), name='hashmabir_list'),
     url(r'^add/$', views.HashmabirCreateView.as_view(), name='hashmabir_create'),
     url(r'^(?P<pk>\d+)/$', views.HashmabirDetailView.as_view(), name='hashmabir_detail'),
