6642dcb3eac8e2c583dac72acb945662e7a56797
Udi Oron <udioron@gmail.com>
2013-10-27 01:16:27 +0300

    changed default db in settings.py to postgres

--- src/h4il/settings.py
+++ src/h4il/settings.py
@@ -19,13 +19,11 @@ EMAIL_SUBJECT_PREFIX = '[Hackita] '
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': './h4il.db',  # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',  # Set to empty string for default.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'h4il',
+        'USER': 'h4il',
+        'PASSWORD': 'h4il',
+        'HOST': 'localhost',
     }
 }
 
@@ -215,6 +213,11 @@ LOGGING = {
             'propagate': True,
             'level': 'DEBUG',
         },
+#         'django.db.backends': {
+#             'handlers': ['console'],
+#             'propagate': True,
+#             'level': 'DEBUG',
+#         },
     }
 }
 
