e93329dcbe16956dc2c5a71ccd9d640482da48b9
Udi Oron <udioron@gmail.com>
2013-11-24 13:32:41 +0200

    Better LMS admin; strikethrough unpublished itmes

--- src/lms/admin.py
+++ src/lms/admin.py
@@ -1,7 +1,39 @@
 from django.contrib import admin
 from lms import models
 
-admin.site.register(models.Trail)
-admin.site.register(models.Item)
+
+class TrailAdmin(admin.ModelAdmin):
+    list_display = (
+                    'title',
+                    'ordinal',
+                    'is_published',
+                    )
+    list_filter = (
+                    'is_published',
+                    )
+    search_fields = (
+                    'title',
+                    )
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = (
+                    'title',
+                    'trail',
+                    'ordinal',
+                    'is_published',
+                    'is_exercise',
+                    )
+    list_filter = (
+                    'trail',
+                    'is_published',
+                    'is_exercise',
+                    )
+    search_fields = (
+                    'title',
+                    )
+
+admin.site.register(models.Trail, TrailAdmin)
+admin.site.register(models.Item, ItemAdmin)
 admin.site.register(models.ItemComment)
 admin.site.register(models.Solution)
