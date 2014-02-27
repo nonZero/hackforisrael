238647165b5c032ca39d5c5c002846e87b8bd475
Udi Oron <udioron@gmail.com>
2014-02-27 22:46:17 +0200

    Add breadcrumbs to project update / admin

--- src/projects/views.py
+++ src/projects/views.py
@@ -1,14 +1,19 @@
 from django.contrib.auth.decorators import permission_required
+from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views.generic import DetailView, ListView, UpdateView
 from extra_views import InlineFormSet, UpdateWithInlinesView
 from projects.models import Project, ProjectMember
 from users.base_views import CommunityOnlyMixin, PermissionRequiredMixin
+from django.utils.translation import ugettext as _
 
 
 class ProjectMixin(CommunityOnlyMixin):
     model = Project
+    breadcrumbs = (
+        (_('Projects'), reverse_lazy('project:list')),
+    )
 
     def get_queryset(self):
         qs = super(ProjectMixin, self).get_queryset()
@@ -34,10 +39,22 @@ class ProjectDetailView(ProjectMixin, DetailView):
     def get_context_data(self, **kwargs):
         d = super(ProjectDetailView, self).get_context_data(**kwargs)
         d['can_edit'] = self.can_edit
+        d['breadcrumbs'] = self.breadcrumbs
+
         return d
 
 
-class ProjectUpdateView(ProjectMixin, PermissionRequiredMixin, UpdateView):
+class SubProjectMixin(ProjectMixin):
+    def get_context_data(self, **kwargs):
+        d = super(SubProjectMixin, self).get_context_data(**kwargs)
+        d['breadcrumbs'] = self.breadcrumbs + (
+            (self.object.title, self.object.get_absolute_url()),
+        )
+
+        return d
+
+
+class ProjectUpdateView(SubProjectMixin, PermissionRequiredMixin, UpdateView):
     def check_permission(self, user):
         return self.can_edit
         #return self.get_object().members.filter(user_id=user.id,
@@ -56,7 +73,7 @@ class MemberInline(InlineFormSet):
     model = ProjectMember
 
 
-class ProjectAdminView(ProjectMixin, PermissionRequiredMixin,
+class ProjectAdminView(SubProjectMixin, PermissionRequiredMixin,
                        UpdateWithInlinesView):
     permission = 'projects.change_project'
 
